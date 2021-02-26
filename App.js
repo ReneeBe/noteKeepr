2f349c72c02e9786f365ba09a41a17af7abe2178
ReneeBe <reneelberger@gmail.com>
2021-02-26 15:01:52 -0800

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,117 @@
 import { StatusBar } from 'expo-status-bar';
+import * as ImagePicker from 'expo-image-picker';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, AccessibilityInfo } from 'react-native';
+import { GOOGLE_CLOUD_VISION_API_KEY } from './secrets';
+
+const API_KEY = GOOGLE_CLOUD_VISION_API_KEY;
+const API_URL = `https://vision.googleapis.com/v1/images:annotate?key=${API_KEY}`;
+
+async function callGoogleVisionAsync(image) {
+  const body = {
+    requests: [
+      {
+        image: {
+          content: image,
+        },
+        features: [
+          // {
+          //   type: 'LABEL_DETECTION',
+          //   maxResults: 1,
+          // },
+          { type: "TEXT_DETECTION", maxResults: 5 },
+          // { type: "DOCUMENT_TEXT_DETECTION", maxResults: 5 },
+        ],
+      },
+    ],
+  };
+
+  const response = await fetch(API_URL, {
+    method: 'POST',
+    // headers: {
+    //   Accept: 'application/json',
+    //   'Content-Type': 'application/json',
+    // },
+    body: JSON.stringify( body )
+  });
+
+  const result = await response.json();
+
+  const pairedDownAllWords = result.responses[0].fullTextAnnotation.pages[0].blocks[0].paragraphs[0].words;
+
+  
+  function wordBuilder (arr) {
+    return arr.reduce((acc, letterObj) => {
+      return acc.concat(letterObj.text);
+    }, []).join('');
+  }
+
+  let theWords = '';
+
+  const textBuilder = (ar) => {
+    for (let i = 0; i < ar.length; i++) {
+      theWords += wordBuilder(ar[i].symbols) + ' ';
+    }
+    return theWords
+  }
+
+  let answer = textBuilder(pairedDownAllWords)
+
+  const allTheWords = [];
+
+  return answer;
+}
 
 export default function App() {
+  const [image, setImage] = React.useState(null);
+  const [status, setStatus] = React.useState(null);
+  const [permissions, setPermissions] = React.useState(false);
+
+  const askPermissionsAsync = async () => {
+    let permissionResult = await ImagePicker.requestCameraPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert('Permission to access camera roll is required!');
+      return;
+    } else {
+      setPermissions(true);
+    }
+  };
+  
+  const takePictureAsync = async () => {
+    const { cancelled, uri, base64 } = await ImagePicker.launchCameraAsync({
+      base64: true,
+    });
+
+    if (!cancelled) {
+      setImage(uri);
+      setStatus('Loading...');
+      try {
+        
+        const result = await callGoogleVisionAsync(base64);
+        setStatus(result);
+
+      } catch (error) {
+        setStatus(`Error: ${error.message}`);
+      }
+    } else {
+      setImage(null);
+      setStatus(null);
+    }
+  };
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {permissions === false ? (
+        <Button onPress={askPermissionsAsync} title="Ask permissions" />
+      ) : (
+        <>
+          {image !== null && <Image style={styles.image} source={{ uri: image }} />}
+          {status !== null && <Text style={styles.text}>{status}</Text>}
+          <Button onPress={takePictureAsync} title="Take a Picture" />
+        </>
+      )}
     </View>
   );
 }
---
+++ todelete.js
@@ -0,0 +1,181 @@
+
+// result Array [
+//     Object {
+//       "fullTextAnnotation": Object { 
+//           "pages": Array [ Object { "blocks": Array [ Object {
+//                 "blockType": "TEXT",
+//                 "boundingBox": Object { "vertices": Array [ Object { "x": 280, "y": 1255,}, Object { "x": 2421, "y": 1526, }, Object { "x": 2367, "y": 1954, }, Object { "x": 226, "y": 1684,},],},
+//                 "paragraphs": Array [
+//                     Object {
+//                     "boundingBox": Object { "vertices": Array [ Object { "x": 280, "y": 1255, }, Object { "x": 2421, "y": 1526, }, Object { "x": 2367, "y": 1954, }, Object { "x": 226, "y": 1684,}, ], },
+//                     "property": Object { "detectedLanguages": Array [ Object { "confidence": 1, "languageCode": "en",},],},
+//                     "words": Array [ Object {
+//                         "boundingBox": Object { "vertices": Array [ Object { "x": 268, "y": 1349, }, Object { "x": 1100, "y": 1454, }, Object { "x": 1063, "y": 1749,}, Object { "x": 231, "y": 1644, }, ], },
+//                         "property": Object { "detectedLanguages": Array [ Object { "languageCode": "en", }, ], },
+//                         "symbols": Array [
+//                             Object {
+//                             "boundingBox": Object { "vertices": Array [ Object { "x": 268, "y": 1349, }, Object { "x": 680, "y": 1401, }, Object { "x": 643, "y": 1694,}, Object { "x": 231,"y": 1642,}, ], },
+//                             "property": Object { "detectedLanguages": Array [ Object { "languageCode": "en",}, ], },
+//                             "text": "C",
+//                             },
+            
+//             Object {
+//             "boundingBox": Object {
+//                 "vertices": Array [
+//                 Object {
+//                     "x": 680,
+//                     "y": 1401,
+//                 },
+//                 Object {
+//                     "x": 822,
+//                     "y": 1419,
+//                 },
+//                 Object {
+//                     "x": 785,
+//                     "y": 1712,
+//                 },
+//                 Object {
+//                     "x": 643,
+//                     "y": 1694,
+//                 },
+//                 ],
+//             },
+//             "property": Object {
+//                 "detectedLanguages": Array [
+//                 Object {
+//                     "languageCode": "en",
+//                 },
+//                 ],
+//             },
+//             "text": "h",
+//             },
+//             Object {
+//             "boundingBox": Object {
+//                 "vertices": Array [
+//                 Object {
+//                     "x": 823,
+//                     "y": 1421,
+//                 },
+//                 Object {
+//                     "x": 965,
+//                     "y": 1439,
+//                 },
+//                 Object {
+//                     "x": 928,
+//                     "y": 1732,
+//                 },
+//                 Object {
+//                     "x": 786,
+//                     "y": 1714,
+//                 },
+//                 ],
+//             },
+//             "property": Object {
+//                 "detectedLanguages": Array [
+//                 Object {
+//                     "languageCode": "en",
+//                 },
+//                 ],
+//             },
+//             "text": "a",
+//             },
+//             Object {
+//             "boundingBox": Object {
+//                 "vertices": Array [
+//                 Object {
+//                     "x": 966,
+//                     "y": 1438,
+//                 },
+//                 Object {
+//                     "x": 1100,
+//                     "y": 1455,
+//                 },
+//                 Object {
+//                     "x": 1063,
+//                     "y": 1748,
+//                 },
+//                 Object {
+//                     "x": 929,
+//                     "y": 1731,
+//                 },
+//                 ],
+//             },
+//             "property": Object {
+//                 "detectedBreak": Object {
+//                 "type": "SPACE",
+//                 },
+//                 "detectedLanguages": Array [
+//                 Object {
+//                     "languageCode": "en",
+//                 },
+//                 ],
+//             },
+//             "text": "t",
+//             },
+//         ],
+//         },
+//         Object {
+//         "boundingBox": Object {
+//             "vertices": Array [
+//             Object {
+//                 "x": 1327,
+//                 "y": 1387,
+//             },
+//             Object {
+//                 "x": 2421,
+//                 "y": 1525,
+//             },
+//             Object {
+//                 "x": 2367,
+//                 "y": 1954,
+//             },
+//             Object {
+//                 "x": 1273,
+//                 "y": 1816,
+//             },
+//             ],
+//         },
+//         "property": Object {
+//             "detectedLanguages": Array [
+//             Object {
+//                 "languageCode": "en",
+//             },
+//             ],
+//         },
+//         "symbols": Array [
+//             Object {
+//             "boundingBox": Object {
+//                 "vertices": Array [
+//                 Object {
+//                     "x": 1327,
+//                     "y": 1388,
+//                 },
+//                 Object {
+//                     "x": 1620,
+//                     "y": 1425,
+//                 },
+//                 Object {
+//                     "x": 1566,
+//                     "y": 1853,
+//                 },
+//                 Object {
+//                     "x": 1273,
+//                     "y": 1816,
+//                 },
+//                 ],
+//             },
+//             "property": Object {
+//                 "detectedLanguages": Array [
+//                 Object {
+//                     "languageCode": "en",
+//                 },
+//                 ],
+//             },
+//             "text": "t",
+//             },
+
+
+//             Object {
+//             "boundingBox": Object {
+//                 "vertices": Array [
+//    ...(truncated to the first 10000 characters)
